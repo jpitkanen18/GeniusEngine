7a8561a469193ffe25baced5d39463e1843cd76b
Jesse Pitkanen <jpitkanen18@gmail.com>
2025-02-19 12:29:58 +0200

    Update utils.hpp

--- source/utils/utils.hpp
+++ source/utils/utils.hpp
@@ -3,16 +3,16 @@
 #include <iostream>
 #include <experimental/filesystem>
 
-using namespace std;
-
 namespace Utils {
+	using std::string, std::experimental::filesystem::current_path, std::cout;
+
 	template<typename T> static void Println(T printable) {
-		cout << printable << endl;
+		cout << printable << '\n';
 	}
 
-	template<typename T> static std::string FileFromSourceDir(T fileName) {
-		std::string stringName(fileName);
-		std::string path = static_cast<std::string>(std::experimental::filesystem::current_path());
+	template<typename T> static string FileFromSourceDir(T fileName) {
+		string stringName(fileName);
+		string path = static_cast<string>(current_path());
 		path.append("/source");
 		path.append(stringName);
 		return path;
